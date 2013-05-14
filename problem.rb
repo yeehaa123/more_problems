f3f61475a4cf8abaf663903516d49e121defb953
Jan Hein Hoogstad <yeehaa@omhp-staff-22-60.wireless.uva.nl>
2013-05-14 18:44:11 +0200

    add lots of features

--- problem.rb
+++ problem.rb
@@ -1,6 +1,9 @@
 require 'dm-core'
 require 'dm-migrations'
 
+
+$authors = []
+$tags = []
 DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.rb")
 
 class Problem
@@ -10,11 +13,33 @@ class Problem
   property :completed, Boolean
   property :title, String
   property :notes, String
-
+  
   def completed?
     !(self.completed.nil? || self.completed == "")
   end
 
+  def categorize
+    words = self.notes.split(" ")
+    words.each do |word|
+      if word.match /[A-Z].+/
+        $authors << word
+      else
+        $tags << word
+      end
+    end
+  end
+end
+
+class Tag
+  attr_accessor :title
+
+  def initialize(title)
+    @title = title
+  end
+
+  def look_up_definition_on_wikipedia
+    puts "To Be Implemented"
+  end
 end
 
 DataMapper.finalize
